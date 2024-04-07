a809a222459015bfa90c0a6d59eec25b5372f9d8
certifiedbadb <you@example.com>
2024-04-07 10:10:20 +0200

    insert

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
@@ -22,9 +23,22 @@ class MovieController extends Controller
         ])
         ->get('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1')
         ->json();
-        
-        foreach ($response[0] as $response_result) {
-            return $response;
-        }
+
+         foreach($response['results'] as $movie){
+
+
+            // $existingrecord = DB::table('movies')::where('name', $movie['title'])->count();
+            // echo $existingrecord;
+//             if ($existingrecord != null) {
+//                 echo "already exists" . "<br>";
+//             }
+//             else{
+            DB::table('movies')->insert(
+                ['name' => $movie['title']]
+            );
+            echo $movie['title'] . " inserted";
+//         }
+         }
+//          echo "are inserted in the database...";
 }
 }
